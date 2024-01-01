1cd17b141140f9e40ab80cc20db82d933a42062d
Subhadeep Banerjee <116381157+subhadeepbanerjee2003@users.noreply.github.com>
2024-01-01 20:12:50 +0530

    Add print statements to pointer1.cpp

--- pointer1.cpp
+++ pointer1.cpp
@@ -3,8 +3,13 @@ using namespace std;
 int main()
 {
  int a = 50;
+ cout << a  << endl;
  int *ptr = &a;
+ cout << ptr  << endl;
+ cout << *ptr  << endl;
  int *q = ptr;
+ cout << q  << endl;
+cout << *q  << endl;
  (*q)++;
  cout << a  << endl;
 return 0;
