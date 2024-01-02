dd2e1b09a66a3c583f34e81b1f44c203b2e4c4f8
Subhadeep Banerjee <116381157+subhadeepbanerjee2003@users.noreply.github.com>
2024-01-02 12:17:50 +0530

    new commit

---
+++ pointer5.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[6] = {1, 2, 3};
+    int *b = a;
+    cout << b[2];
+
+    /*int a[] = {1, 2, 3, 4};
+    int *p = a++;
+    cout << *p << endl;*/
+
+    return 0;
+}
